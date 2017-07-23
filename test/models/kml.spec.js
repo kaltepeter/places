fc72496eeaef4562637f120dad044828ba44233e
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2017-07-22 23:21:32 -0500

    adding crud for kml model

--- test/models/kml.spec.js
+++ test/models/kml.spec.js
@@ -1,7 +1,7 @@
 const should = require('should');
 const DB = require('../../lib/db.js');
 const fixtures = require('../fixtures/model-kml.json');
-const kml = require('../../lib/models/kml');
+const Kml = require('../../lib/models/kml');
 
 describe('Model: kml', () => {
 
@@ -17,9 +17,33 @@ describe('Model: kml', () => {
   });
 
   it('all', (done) => {
-    kml.all((err, kml) => {
-      kml.length.should.eql(1);
+    Kml.all((err, kml) => {
+      kml.length.should.eql(2);
       done();
     });
+  });
+
+  it('create', (done) => {
+    Kml.create('The South', '9e4f2ec12009dab5702e3aff56621e81', (err, id) => {
+      Kml.all((err, kml) => {
+        kml.length.should.eql(3);
+        kml[2]._id.should.eql(id);
+        kml[2].name.should.eql('The South');
+        kml[2].path.should.eql('9e4f2ec12009dab5702e3aff56621e81');
+        done();
+      })
+    })
+  });
+
+  it('remove', (done) => {
+    Kml.all((err, kmls) => {
+      Kml.remove(kmls[0]._id, (err) => {
+        Kml.all((err, result) => {
+          result.length.should.eql(1);
+          result[0]._id.should.not.eql(kmls[0]._id);
+          done();
+        })
+      })
+    })
   });
 });
